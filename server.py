bc35801ce3e82bf262f6d7b84eff5b4f51959ea6
mai-python <rlagustn4588@gmail.com>
2025-03-16 23:40:14 +0900

    Update server.py

--- server.py
+++ server.py
@@ -1,6 +1,8 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS  # CORS를 import
 
 app = Flask(__name__)
+CORS(app)  # CORS 설정 추가
 
 @app.route('/get_command', methods=['POST'])
 def get_command():
